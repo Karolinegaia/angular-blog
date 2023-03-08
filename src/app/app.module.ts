36b895674e7831f91fe5e60de14d0eaf0184fba6
Karoline Gaia <drakarolinegaia@gmail.com>
2023-03-08 13:38:00 -0300

    Updating the Big-Card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
     MenuTitleComponent,
-    BigCardComponent
+    BigCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
